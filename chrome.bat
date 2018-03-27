83813fe7e32347acbc22a6fe95d4ff1cbbd57a11
Ayo Ayco <mail@ayoayco.com>
2018-03-27 09:16:53 +0800

    update chrome bat

--- chrome.bat
+++ chrome.bat
@@ -1,4 +1,4 @@
 :: start "" cmd /c 
 :: start "" cmd /c node webserver.js 8080 ../
 :: start "" cmd /c node proxy.js 8081 sestvwm3e11.infor.com 23007
-"c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --disable-web-security --user-data-dir -incognito "http://localhost:8080/mne/indexDev.html"
+"c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --disable-web-security --user-data-dir -incognito "http://localhost:4200"
